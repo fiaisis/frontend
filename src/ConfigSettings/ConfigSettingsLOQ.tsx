48cc1b44e865f2228827af37dc9cd43adb4b74b4
HumzahJavid <25638011+HumzahJavid@users.noreply.github.com>
2024-10-30 12:03:35 +0000

    Remove temporary button (and functionality) to view extras contents

--- src/ConfigSettings/ConfigSettingsLOQ.tsx
+++ src/ConfigSettings/ConfigSettingsLOQ.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 // Material UI components
 import { Box, Button } from '@mui/material';
 import { UploadFile, Edit } from '@mui/icons-material';
-import useFetchData from '../useFetchData';
 
 // Local data
 import ConfigSettingsGeneral from './ConfigSettingsGeneral';
@@ -17,8 +16,6 @@ const ConfigSettingsLOQ: React.FC = () => {
   const { instrumentName } = useParams<{ instrumentName: string }>();
   const instrument_url = `${fiaApiUrl}/extras/${instrumentName.toLowerCase()}`;
   const { response, handleFileUpload } = FileUpload(instrument_url);
-
-  const { data, loading, fetchData } = useFetchData(`${fiaApiUrl}/extras`);
   // Insert LOQ specifc buttons into the ConfigSettingsGeneral component
   return (
     <ConfigSettingsGeneral>
@@ -29,11 +26,6 @@ const ConfigSettingsLOQ: React.FC = () => {
         </Button>
         {/* replace with a snackbar/toastlike component */}
         {response && <pre>{JSON.stringify(response.data, null, 2)}</pre>}
-        <Button component="label" variant="contained" onClick={fetchData}>
-          View extras...
-        </Button>
-        {loading && <p>Loading...</p>}
-        {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
       </Box>
 
       {/* Change script button -- disabled for now */}
