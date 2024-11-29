5d54f52eece2adfe12e4262f8fe906a4db0c2b60
HumzahJavid <25638011+HumzahJavid@users.noreply.github.com>
2024-11-29 13:53:47 +0000

    Add Job start and end columns to job table (#383)
    * Add Job start and end columns to job table
    
    To allow for sorting by job start and end times
    
    * Fix order of job start and end data cells
    
    Not in matching positions relative to table headers

--- src/Jobs/JobsBase.tsx
+++ src/Jobs/JobsBase.tsx
@@ -424,6 +424,8 @@ const JobsBase: React.FC<JobsBaseProps> = ({
           <TableCell sx={{ width: '10%' }}>{extractFileName(job.run?.filename || 'N/A')}</TableCell>
           <TableCell sx={{ width: '15%' }}>{formatDateTime(job.run?.run_start || 'N/A')}</TableCell>
           <TableCell sx={{ width: '15%' }}>{formatDateTime(job.run?.run_end || 'N/A')}</TableCell>
+          <TableCell sx={{ width: '15%' }}>{job.start ? formatDateTime(job.start) : 'N/A'}</TableCell>
+          <TableCell sx={{ width: '15%' }}>{job.end ? formatDateTime(job.end) : 'N/A'}</TableCell>
           <TableCell sx={{ width: '32%' }}>{job.run?.title || 'N/A'}</TableCell>
           {customRowCells && customRowCells(job)}
         </TableRow>
@@ -656,6 +658,12 @@ const JobsBase: React.FC<JobsBaseProps> = ({
                   </TableCell>
                   <TableCell sx={{ width: '15%', ...headerStyles(theme) }} onClick={() => handleSort('run_end')}>
                     Run end {orderBy === 'run_end' ? (orderDirection === 'asc' ? '↑' : '↓') : ''}
+                  </TableCell>
+                  <TableCell sx={{ width: '15%', ...headerStyles(theme) }} onClick={() => handleSort('start')}>
+                    Job start {orderBy === 'start' ? (orderDirection === 'asc' ? '↑' : '↓') : ''}
+                  </TableCell>
+                  <TableCell sx={{ width: '15%', ...headerStyles(theme) }} onClick={() => handleSort('end')}>
+                    Job end {orderBy === 'end' ? (orderDirection === 'asc' ? '↑' : '↓') : ''}
                   </TableCell>
                   <TableCell sx={{ width: '32%', ...headerStyles(theme) }}>Title</TableCell>
                   {customHeaders && customHeaders}
