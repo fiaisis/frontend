dd6b00e9debd273aeb00078b32acdb17a96ea4eb
HumzahJavid <25638011+HumzahJavid@users.noreply.github.com>
2024-12-10 11:18:36 +0000

    Add state to file upload message (selected or uploaded)
    To make it clear when the file has been uploaded

--- src/ConfigSettings/ConfigSettingsLOQ.tsx
+++ src/ConfigSettings/ConfigSettingsLOQ.tsx
@@ -13,13 +13,16 @@ const fiaApiUrl = process.env.REACT_APP_FIA_REST_API_URL;
 
 const ConfigSettingsLOQ: React.FC = () => {
   const instrument_url = `${fiaApiUrl}/extras/loq`;
-  const [selectedFile, setSelectedFile] = useState<File | null>(null);
   // Insert LOQ specifc buttons into the ConfigSettingsGeneral component
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [uploadMessage, setUploadMessage] = useState<string>('');
 
   // File selection
   const handleFileSelection = (event: React.ChangeEvent<HTMLInputElement>): void => {
     const file = event.target.files ? event.target.files[0] : null;
     setSelectedFile(file);
+    const newUploadMessage = file ? `Selected file: ${file.name}` : '';
+    setUploadMessage(newUploadMessage);
   };
 
   // Callback for uploading the selected file
@@ -30,6 +33,7 @@ const ConfigSettingsLOQ: React.FC = () => {
       const formData = new FormData();
       formData.append('file', selectedFile);
       await axios.post(`${instrument_url}/${selectedFile.name}`, formData);
+      setUploadMessage(`Uploaded file: ${selectedFile.name}`);
     } catch (error) {
       console.error('Error uploading file:', error);
       throw new Error('Failed to upload the file');
@@ -41,14 +45,9 @@ const ConfigSettingsLOQ: React.FC = () => {
       <Box sx={{ mb: 2 }}>
         <Button component="label" variant="contained" startIcon={<UploadFile />}>
           Upload file...
-          <input type="file" multiple hidden onChange={(event) => handleFileSelection(event)} />
+          <input type="file" multiple hidden onChange={handleFileSelection} />
         </Button>
-        {selectedFile && (
-          // Make this message dynamic?
-          <Box sx={{ mt: 1 }}>
-            Selected file: <strong>{selectedFile.name}</strong>
-          </Box>
-        )}
+        {selectedFile && <Box sx={{ mt: 1 }}>{uploadMessage}</Box>}
       </Box>
 
       {/* Change script button -- disabled for now */}
