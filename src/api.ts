641f34582abc33da7a012fee1f4936840b065d69
Keiran Price <keiran.price@stfc.ac.uk>
2025-01-07 08:30:43 +0000

    Add initial api object

---
+++ src/api.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+const isDev = process.env.REACT_APP_DEV_MODE === 'true';
+export const fiaApi = axios.create({
+  baseURL: process.env.REACT_APP_FIA_REST_API_URL,
+  headers: { Authorization: `Bearer ${!isDev ? localStorage.getItem('scigateway:token') : ''}` },
+});
+
+// let isFetchingAccessToken = false;
+// let failedAuthRequestQueue: ((shouldReject?: boolean) => void)[] = [];
+
+/* This should be called when SciGateway successfully refreshes the access token - it retries
+   all requests that failed due to an invalid token */
+// export const retryFailedAuthRequests = (): void => {
+//   isFetchingAccessToken = false;
+//   failedAuthRequestQueue.forEach((callback) => callback());
+//   failedAuthRequestQueue = [];
+// };
+//
+// /* This should be called when SciGateway logs out as would occur if a token refresh fails
+//    due to the refresh token being out of date - it rejects all active request promises that
+//    were awaiting a token refresh using the original error that occurred on the first attempt */
+// export const clearFailedAuthRequestsQueue = (): void => {
+//   isFetchingAccessToken = false;
+//   failedAuthRequestQueue.forEach((callback) => callback(true));
+//   failedAuthRequestQueue = [];
+// };
+
+fiaApi.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.log('hello');
+    console.log(error.config);
+  }
+);
