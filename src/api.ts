b035cbdbd07a079fade054c6b78cc50946a3d303
Keiran Price <keiran.price@stfc.ac.uk>
2025-01-16 12:07:28 +0000

    Minor changes

--- src/api.ts
+++ src/api.ts
@@ -1,21 +1,23 @@
 import axios from 'axios';
+
 const isDev = process.env.REACT_APP_DEV_MODE === 'true';
+
 export const fiaApi = axios.create();
 
 let isFetchingAccessToken = false;
 let failedAuthRequestQueue: ((shouldReject?: boolean) => void)[] = [];
 
-/* This should be called when SciGateway successfully refreshes the access token - it retries
-   all requests that failed due to an invalid token */
+//  This should be called when SciGateway successfully refreshes the access token - it retries
+//  all requests that failed due to an invalid token
 export const retryFailedAuthRequests = (): void => {
   isFetchingAccessToken = false;
   failedAuthRequestQueue.forEach((callback) => callback());
   failedAuthRequestQueue = [];
 };
 
-/* This should be called when SciGateway logs out as would occur if a token refresh fails
-   due to the refresh token being out of date - it rejects all active request promises that
-   were awaiting a token refresh using the original error that occurred on the first attempt */
+// This should be called when SciGateway logs out as would occur if a token refresh fails
+// due to the refresh token being out of date - it rejects all active request promises that
+//  were awaiting a token refresh using the original error that occurred on the first attempt
 export const clearFailedAuthRequestsQueue = (): void => {
   isFetchingAccessToken = false;
   failedAuthRequestQueue.forEach((callback) => callback(true));
@@ -30,8 +32,8 @@ fiaApi.interceptors.request.use(async (config) => {
   return config;
 });
 
-/* This should be called when SciGateway successfully refreshes the access token - it retries
-   all requests that failed due to an invalid token */
+// This should be called when SciGateway successfully refreshes the access token - it retries
+//    all requests that failed due to an invalid token
 fiaApi.interceptors.response.use(
   (response) => response,
   (error) => {
