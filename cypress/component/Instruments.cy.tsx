9bcb440f8a8260717411bdcef44edbe03033a5ed
Keiran Price <keiran.price@stfc.ac.uk>
2025-02-21 08:55:26 +0000

    Refactor Instruments component file structure.
    Moved Instruments component and updated imports to align with the new directory structure. This enhances code organization and simplifies the pathing for related files.

--- cypress/component/Instruments.cy.tsx
+++ cypress/component/Instruments.cy.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Instruments from '../../src/Instruments';
+import Instruments from '../../src/pages/Instruments';
 import { mount } from '@cypress/react';
 
 describe('<Instruments />', () => {
