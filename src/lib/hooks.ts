2babc708bb23c22b44f5a3f737fe68cdf55033f9
Keiran Price <keiran.price@stfc.ac.uk>
2025-02-21 08:53:28 +0000

    create lib

---
+++ src/lib/hooks.ts
@@ -0,0 +1,30 @@
+import React, { useCallback } from 'react';
+import { fiaApi } from './api';
+
+import { Job } from './types';
+
+export const useFetchJobs = (
+  apiPath: string,
+  queryParams: string,
+  setJobs: React.Dispatch<React.SetStateAction<Job[]>>
+): (() => Promise<void>) => {
+  return useCallback(async () => {
+    fiaApi
+      .get(`${apiPath}?${queryParams}`)
+      .then((res) => res.data)
+      .then((jobs) => setJobs(jobs))
+      .catch((err) => console.error('Error fetching jobs:', err));
+  }, [apiPath, queryParams, setJobs]);
+};
+
+export const useFetchTotalCount = (
+  apiPath: string,
+  setTotalRows: React.Dispatch<React.SetStateAction<number>>
+): (() => Promise<void>) => {
+  return useCallback(async () => {
+    fiaApi
+      .get(apiPath)
+      .then((res) => setTotalRows(res.data.count))
+      .catch((err) => console.error('Error fetching row count', err.message));
+  }, [apiPath, setTotalRows]);
+};
