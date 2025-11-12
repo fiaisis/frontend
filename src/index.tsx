e02f8f28b08444559a6368559654723146d61b87
esmithExperimentControls <evan.smith@stfc.ac.uk>
2025-11-12 16:40:38 +0000

    Update texts to make the tour complete.

--- src/index.tsx
+++ src/index.tsx
@@ -63,9 +63,9 @@ const reactLifecycles = singleSpaReact({
 // 4. How high up in the section should your link be - ascending order
 // 5. Help text renders a tooltip in the site tour for this link
 // 6. Whether the link should be visible to unauthenticated users
-createRoute('Reductions', 'Home page', '/fia', 1, 'Data help text', false);
-createRoute('Reductions', 'Instruments', '/fia/instruments', 2, 'Data help text', false);
-createRoute('Reductions', 'Reduction history', '/fia/reduction-history', 3, 'Data help text', false);
+createRoute('Reductions', 'Home page', '/fia', 1, 'Click here to go back to the Homepage', true);
+createRoute('Reductions', 'Instruments', '/fia/instruments', 2, 'Click here to see all instruments', true);
+createRoute('Reductions', 'Reduction history', '/fia/reduction-history', 3, 'Click here to see all reductions', true);
 
 // Single-SPA bootstrap methods have no idea what type of inputs may be
 // pushed down from the parent app
