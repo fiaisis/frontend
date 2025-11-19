01c402cbcd92dd770f9f5a0aa913914eccb2dc4b
Samuel <47388695+Dagonite@users.noreply.github.com>
2025-11-19 10:00:13 +0000

    Update tooltip text for route creation

--- src/index.tsx
+++ src/index.tsx
@@ -67,8 +67,8 @@ const reactLifecycles = singleSpaReact({
 // 5. Help text renders a tooltip in the site tour for this link
 // 6. Whether the link should be visible to unauthenticated users
 createRoute('Reductions', 'Home page', '/fia', 1, '', false);
-createRoute('Reductions', 'Instruments', '/fia/instruments', 2, 'Click here to see a list of all Instruments', false);
-createRoute('Reductions', 'Reduction history', '/fia/reduction-history', 3, 'Click here to see a list of all reductions', false);
+createRoute('Reductions', 'Instruments', '/fia/instruments', 2, 'Click here to see a list of all Instruments.', false);
+createRoute('Reductions', 'Reduction history', '/fia/reduction-history', 3, 'Click here to see a list of all reductions.', false);
 
 // Single-SPA bootstrap methods have no idea what type of inputs may be
 // pushed down from the parent app
