36f048ed8b0ce960cefb6242100f990c5a3437f8
Keiran Price <keiran.price@stfc.ac.uk>
2026-03-31 12:23:07 +0100

    Remove `apiUrl` parameter from query URL utility functions

--- src/components/data-viewer/utils/TextQueryUrl.tsx
+++ src/components/data-viewer/utils/TextQueryUrl.tsx
@@ -1,17 +1,12 @@
-export function TextQueryUrl(
-  apiUrl: string,
-  instrument?: string,
-  experimentNumber?: string,
-  userNumber?: string
-): string | null {
+export function TextQueryUrl(instrument?: string, experimentNumber?: string, userNumber?: string): string | null {
   if (instrument != null && experimentNumber != null) {
-    return `${apiUrl}/text/instrument/${instrument}/experiment_number/${experimentNumber}`;
+    return `/text/instrument/${instrument}/experiment_number/${experimentNumber}`;
   }
   if (userNumber != null) {
-    return `${apiUrl}/text/generic/user_number/${userNumber}`;
+    return `/text/generic/user_number/${userNumber}`;
   }
   if (experimentNumber != null) {
-    return `${apiUrl}/text/generic/experiment_number/${experimentNumber}`;
+    return `/text/generic/experiment_number/${experimentNumber}`;
   }
   return null;
 }
